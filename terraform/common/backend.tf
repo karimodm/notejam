1ddd5698d0deecb22a66e091640e036953824362
Andrea Villa <amavilla.89@gmail.com>
2017-09-22 15:34:44 +0200

    Cleanup

--- terraform/common/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-notejam"
-    key    = "common"
-    region = "eu-central-1"
-  }
-}
--- terraform/deploy/output.tf
+++
@@ -1,7 +0,0 @@
-/**
-  * DNS name from ELB
-  * In production, you can add this DNS Name to Route 53 (your domain)
-  */
-output "elb_dns_name" {
-    value = "${aws_elb.main.dns_name}"
-}
